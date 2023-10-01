8f6d16f501a7372dcfdbdfd8bca6d2611f26a5e0
Dmitry <a1@macbook-air-1.local>
2023-10-01 21:53:20 +0300

    (feat:add js file, create clear button, when user click for button, shes clear input '(Sun, Oct 1, 2023 21:53:44 PM) '

--- image-galery/assets/js/index.js
+++ image-galery/assets/js/index.js
@@ -1,1 +1,15 @@
-console.log('hello')
+// clear btn
+const btnSearch = document.querySelector('.search__button');
+const btnClose = document.querySelector('.search__button_close');
+const input = document.querySelector('.search__input');
+
+alert('Привет, я не до конца разобрался с API для картинок, но я точно это сделаю до конца кросс чека, если ты дашь мне такую возможнось. На данный момент, ты можешь оценить мою работу как пожелаешь, если у тебя потом появится свободная минутка, что бы перепроверить ее, я буду безмерно благодарен. Если ты не против то оставь свой аккаунт открытым, что бы я мог тебе написать, огромное спасибо!');
+
+function setFocus () {
+    input.focus();
+}
+
+btnClose.addEventListener('click', () => {
+    event.preventDefault()
+    input.value = '';
+});
