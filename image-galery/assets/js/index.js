d9f20ee78f7636853d191591a79a5a620bba11ca
Dmitry <a1@macbook-air-1.local>
2023-10-03 08:26:46 +0300

    feat: add correct js file '(Oct, Tue 3, 2023 08:26:32 PM)'

--- image-galery/assets/js/index.js
+++ image-galery/assets/js/index.js
@@ -2,14 +2,55 @@
 const btnSearch = document.querySelector('.search__button');
 const btnClose = document.querySelector('.search__button_close');
 const input = document.querySelector('.search__input');
-
-alert('Привет, я не до конца разобрался с API для картинок, но я точно это сделаю до конца кросс чека, если ты дашь мне такую возможнось. На данный момент, ты можешь оценить мою работу как пожелаешь, если у тебя потом появится свободная минутка, что бы перепроверить ее, я буду безмерно благодарен. Если ты не против то оставь свой аккаунт открытым, что бы я мог тебе написать, огромное спасибо!');
+const containerImg = document.querySelector('.container__img');
 
 function setFocus () {
     input.focus();
 }
 
+// const imgArr = ['path1', 'path2', 'path3','path4', 'path5'];
+
+const imgArr = [
+    {
+        imgPath: 'path1',
+    },
+    {
+        imgPath: 'path2',
+    },
+    {
+        imgPath: 'path3',
+    },
+    {
+        imgPath: 'path4'
+    },
+    {
+        imgPath: 'path5',
+    },
+]
+
+function addImgToHtml (imgArr) {
+    for (let i = 0; i <= imgArr.length; i +=1 ) {
+        containerImg.innerHTML += `<div class="img__wrapper">
+                                    <img class="img__content" src=${imgArr[i]} alt="photo">
+                                </div>`
+    }
+}
+
+addImgToHtml(imgArr);
+
+
+input.addEventListener("keyup", function(event){
+    if(event.keyCode === 13) {
+     btnSearch.click();
+     console.log('hi')
+    }
+})
+
 btnClose.addEventListener('click', () => {
     event.preventDefault()
     input.value = '';
+});
+
+btnSearch.addEventListener('click', () => {
+    event.preventDefault()
 });
