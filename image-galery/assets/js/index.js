9ecd0089ea00b0205d9068c56dd82ad2419ca0c8
Dmitry <a1@macbook-air-1.local>
2023-10-04 10:14:16 +0300

    feat: add worked API for site '(Oct, Wed 04, 2023 10:14:10 PM )'

--- image-galery/assets/js/index.js
+++ image-galery/assets/js/index.js
@@ -1,48 +1,63 @@
 // clear btn
+const CLIENT_ID = "Sn_s3gHw5E_z4kvsMct4V0ObVe0JgJot1sEYTJFPC4w";
+const baseUrl = `https://api.unsplash.com/`;
+
+function addImgToHtml(imgArr) {
+    containerImg.innerHTML = '';
+    for (let i = 0; i < imgArr.length; i++) {
+        containerImg.innerHTML += `<div class="img__wrapper">
+                                    <img class="img__content" src=${imgArr[i].urls.regular} alt=${imgArr[i].alt_description}>
+                                </div>`
+    }
+}
+
+const getPhotos = async () => {
+    const response = await fetch(`${baseUrl}/photos?page=1`, {
+        method: 'GET',
+        withCredentials: true,
+        headers: {
+            'Authorization': `Client-ID ${CLIENT_ID}`,
+            'Content-Type': 'application/json'
+        }})
+
+
+    const data = await response.json();
+    addImgToHtml(data)
+}
+
+getPhotos();
+
+const getPhotosByQuery = async (search) => {
+    const response = await fetch(`${baseUrl}/search/photos?page=1&query=${search}`, {
+        method: 'GET',
+        withCredentials: true,
+        headers: {
+            'Authorization': `Client-ID ${CLIENT_ID}`,
+            'Content-Type': 'application/json'
+        }})
+
+
+    const data = await response.json();
+    addImgToHtml(data.results)
+}
+
+//a716CLgGaFTt8P7VHvNYHLhctIk5wG7WHwajfCikm5c
 const btnSearch = document.querySelector('.search__button');
 const btnClose = document.querySelector('.search__button_close');
 const input = document.querySelector('.search__input');
 const containerImg = document.querySelector('.container__img');
 
-function setFocus () {
+function setFocus() {
     input.focus();
 }
 
-// const imgArr = ['path1', 'path2', 'path3','path4', 'path5'];
 
-const imgArr = [
-    {
-        imgPath: 'path1',
-    },
-    {
-        imgPath: 'path2',
-    },
-    {
-        imgPath: 'path3',
-    },
-    {
-        imgPath: 'path4'
-    },
-    {
-        imgPath: 'path5',
-    },
-]
-
-function addImgToHtml (imgArr) {
-    for (let i = 0; i <= imgArr.length; i +=1 ) {
-        containerImg.innerHTML += `<div class="img__wrapper">
-                                    <img class="img__content" src=${imgArr[i]} alt="photo">
-                                </div>`
-    }
-}
-
-addImgToHtml(imgArr);
-
-
-input.addEventListener("keyup", function(event){
-    if(event.keyCode === 13) {
-     btnSearch.click();
-     console.log('hi')
+  input.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        btnSearch.click();
+        if (input.value) {
+            getPhotosByQuery(input.value);
+        }
     }
 })
 
