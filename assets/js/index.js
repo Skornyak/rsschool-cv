a442e2d8dc514a5a90494305936f333904123ef6
Skornyak <dieharddimon@gmail.com>
2024-07-02 19:25:08 +0300

    feat: add new file in progect, add img for progect

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,18 @@
-const changeThemBtn = document.querySelector('.change__them_btn');
-const body = document.querySelector('.body');
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
 
-const addClass = () => {
-    body.classList.toggle('day-them')
-};
+  });
 
-changeThemBtn.addEventListener('click', addClass);
+// const changeThemBtn = document.querySelector('.change__them_btn');
+// const body = document.querySelector('.body');
+
+// const addClass = () => {
+//     body.classList.toggle('day-them')
+// };
+
+// changeThemBtn.addEventListener('click', addClass);
